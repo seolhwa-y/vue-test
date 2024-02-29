5e93474e91239feb5c1331de4bad247dc809ed33
SeolHwa <103579961+seolhwa-y@users.noreply.github.com>
2024-02-29 17:23:47 +0900

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+ set -e
 
 # build
-npm run build
+ npm run build
 
 # navigate into the build output directory
-#cd dist
+ cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+ echo 'shy-cherish.com' > CNAME
 
-#git init
-#git add -A
-#git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+ git push -f git@github.com:seolhwa-y/seolhwa-y.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+ git push -f git@github.com:seolhwa-y/vue-test.git main:gh-pages
 
-#cd -
+cd -
