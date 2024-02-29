ec46e0ae26329b12302c4bd79adab9b58a726b92
SeolHwa <103579961+seolhwa-y@users.noreply.github.com>
2024-02-29 21:01:29 +0900

    npm run build

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  // devServer: {
-  //     proxy: {
-  //         '/': {
-  //             target: 'http://LOCALHOST',
-  //             pathRewrite: { '^/': '' },
-  //             changeOrigin: true,
-  //             secure: false,
-  //         },
-  //     },
-  // },
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/vue-test/" // GitHub Pages의 프로젝트 이름에 따라 경로를 수정하세요.
-      : "/",
-  outputDir: "docs",
-  //   configureWebpack: (config) => {
-  //     config.output.filename = "js/[name].[chunkhash].js";
-  //     config.output.chunkFilename = "js/[name].[chunkhash].js";
-  //   },
+    // devServer: {
+    //     proxy: {
+    //         '/': {
+    //             target: 'http://LOCALHOST',
+    //             pathRewrite: { '^/': '' },
+    //             changeOrigin: true,
+    //             secure: false,
+    //         },
+    //     },
+    // },
+    publicPath:
+        process.env.NODE_ENV === 'production'
+            ? 'https://seolhwa-y.github.io/vue-test/' // GitHub Pages의 프로젝트 이름에 따라 경로를 수정하세요.
+            : '/',
+    outputDir: 'docs',
+    //   configureWebpack: (config) => {
+    //     config.output.filename = "js/[name].[chunkhash].js";
+    //     config.output.chunkFilename = "js/[name].[chunkhash].js";
+    //   },
 };
 
 // 요청하는 URL을 부를 때 localhost가 server와 통신할 때 에러가 나기 때문에
